7023dcf53f8a9b29480eb4d4dd7886879f6c5ac6
mf13-mra <mf13@kent.ac.uk>
2026-03-26 20:44:57 +0000

    OOP...

--- OOP.py
+++ OOP.py
@@ -13,12 +13,35 @@ class student(person):
         super().__init__(name, age) #constructor from super class
         self.graduationyear = graduationyear
 
-        def sayGraduation(self):
-            print(f"hello i graduate in {self.graduationyear} and my name is {self.name}")
+    def sayGraduation(self):
+            print(f"hello i graduate in {self.graduationyear} and my name is {self.name}!")
 
+#another subclass
+class teacher(person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
 
+    def calculateYearlySalary(self):
+        yearlySalary = self.salary * 12
+        return yearlySalary
+    
+    def teach(self, student : student): #2 different subclasses interacting with eachother!
+        print(f"hello I am {self.name}, and today I will teach {student.name} about something cool!")
 
-            
+student1 = student("Shalissa", 18, 2030)
 
+teacher1 = teacher("Mr.Tank", 27, 1500)
 
-        
+student1.sayGraduation()
+student1.greet()
+
+#student1.calculateYearlySalary() would return an error because teacher is a different subclass :0
+#just like the teacher1.saysGraduation() would return an error
+
+teacher1.greet()
+print(f"I make {teacher1.calculateYearlySalary()} a year!")
+
+del student1 #you can delete them too
+#doing anything with the student1 object would cause error because non-existent
+
